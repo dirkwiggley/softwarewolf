c315a074b5913879b67ce11c2ba3ec5b0ecf9364
Dirk Wiggley <dirkwiggley@gmail.com>
2026-09-03 09:32:13 -0700

    Updated configurations to build and run prod and dev versions

--- apps/docs/src/db.ts
+++ apps/docs/src/db.ts
@@ -1,5 +1,9 @@
-import { PrismaClient } from './generated/client/index.js';
+// Import the entire file namespace using wildcard syntax
+import * as prismaModule from './generated/client/index.js';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+
+// Safely grab the constructor out of the compiled namespace keys
+const { PrismaClient } = prismaModule;
 
 // Prisma 7's MariaDB adapter accepts your database URL string directly as its primary argument!
 const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
